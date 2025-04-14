374462a84f95cd748b936e6c8f4bd2a5af5b6609
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-04-14 20:53:22 -0300

    Update EmpregadoComissionado.java

--- src/EmpregadoComissionado.java
+++ src/EmpregadoComissionado.java
@@ -2,8 +2,19 @@ public class EmpregadoComissionado extends Empregado {
     double totalDeVendas;
     double comissao;
 
+    public EmpregadoComissionado(long matricula, String nome, double comissao, double totalDeVendas) {
+        // chama o construtor da superclasse
+        super(matricula, nome);
+        this.comissao = comissao;
+        this.totalDeVendas = totalDeVendas;
+    }
+
     public double calcularSalario() {
-        //return totalDeVendas * comissao / 100;
-        return 1;
+        return totalDeVendas * comissao / 100;
+    }
+
+    @Override
+    public String getDados() {
+        return super.getDados() + " --- " + comissao + " --- " + totalDeVendas ;
     }
 }
