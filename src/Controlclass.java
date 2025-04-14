65874c9a18b5c276f6c63ccafe9b2712fc9c6c0b
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-04-14 20:53:14 -0300

    Create Controlclass.java

---
+++ src/Controlclass.java
@@ -0,0 +1,24 @@
+public class Controlclass {
+    private Empregado[] empregados = new Empregado[3];
+    private int index = 0;
+
+    public void inserir(Empregado empregado){
+        this.empregados[index++] = empregado;
+    }
+
+    public Empregado pesquisar(long matricula){
+        for (int i = 0; i < index; i++){
+            if (empregados[i].matricula == matricula){
+                return empregados[i];
+            }
+        } return null;
+    }
+
+    public String listar(){
+        String aux = "";
+        for (int i = 0; i < index; i++){
+            aux += empregados[i] + "\n";
+            aux += "Salário R$ " + empregados[i].calcularSalario();
+        }
+    }
+}
