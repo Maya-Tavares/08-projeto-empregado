11400773ea50121899195effc06df6dfe39edc2e
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-04-14 20:53:17 -0300

    Create Controle.java

---
+++ src/Controle.java
@@ -0,0 +1,47 @@
+public class Controle {
+//
+//    private void menuUsario() {
+//        int opcao;
+//        String menu = "1. Criar novo\n2. Pesquisar\n3.Listar\n4.Sair";
+//        do {
+//            opcao = parseInt(showInputDialog(menu));
+//            if (opcao < 1 || opcao > 4) {
+//                showMessageDialog(null, "Opção inválida.");
+//            } else {
+//                switch (opcao) {
+//                    case 1:
+//                        inserirEmpregado();
+//                        break;
+//                    case 2:
+//                        pesquisar();
+//                        break;
+//                    case 3:
+//                        listar();
+//                        break;
+//                    default:
+//                        break;
+//                }
+//            }
+//        } while (opcao != 4);
+//    }
+//
+//
+//    private void inserirEmpregado(){
+//        String nome;
+//        long matricula;
+//
+//
+//    }
+//    private void pesquisar(){
+//
+//
+//    }
+//
+//    private void listar(){
+//
+//
+//
+//    }
+//
+//
+}
