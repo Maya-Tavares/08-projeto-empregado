0451f31fdc016c65b656904de37ab0a03d8824eb
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-04-14 20:53:20 -0300

    Update Empregado.java

--- src/Empregado.java
+++ src/Empregado.java
@@ -2,6 +2,15 @@ public abstract class Empregado {
     long matricula;
     String nome;
 
+    public Empregado(long matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
     public abstract double calcularSalario();
+
+    public String getDados(){
+        return matricula + " -- " + nome;
+    }
 
 }
